f43618e8c976bd24168dc751fe14ece61ccb43d0
Pedro Calixto <pgcalixto@gmail.com>
2017-09-09 16:13:16 -0300

    Documented parts of the Matlab code
    For a better understanding of what is what, comments were added to some
    Matlab
    code parts.

--- Lab 01/exp01e03.m
+++ Lab 01/exp01e03.m
@@ -1,6 +1,8 @@
 
-% questao 3
-% item a
+% QUESTAO 3
+% ITEM a
+
+% parametros iniciais
 s = tf('s');
 
 mc1 = 0.778;
@@ -14,23 +16,39 @@ khw = 14732;
 k1 = 338.6;
 deltak1 = 361.4;
 
+% Gps eh a funcao de transferencia da "planta compensada" para o problema servo
+% GpsDelta eh Gps*, a funcao de transferencia para o valor perturbado
 Gps = khw / (m1*s^2 + (c1+khw*kv)*s + k1);
 GpsDelta = khw / (m1*s^2 + (c1+khw*kv)*s + (k1 + deltak1));
 
+display(tf(Gps));
+display(tf(GpsDelta));
+
+% ganhos de baixa frequencia
 Gps0 = dcgain(Gps);
 Gps0Delta = dcgain(GpsDelta);
-
-% item b
-Gpr = 1 / (m1*s^2 + (c1+khw*kv)*s + k1);
-GprDelta = 1 / (m1*s^2 + (c1+khw*kv)*s + (k1 + deltak1));
-
-Gpr0 = dcgain(Gpr);
-Gpr0Delta = dcgain(GprDelta);
 
 display(Gps0);
 display(Gps0Delta);
 
-% item d
+% ITEM b
+Gpr = 1 / (m1*s^2 + (c1+khw*kv)*s + k1);
+GprDelta = 1 / (m1*s^2 + (c1+khw*kv)*s + (k1 + deltak1));
+
+display(tf(Gpr));
+display(tf(GprDelta));
+
+Gpr0 = dcgain(Gpr);
+Gpr0Delta = dcgain(GprDelta);
+
+display(Gpr0);
+display(Gpr0Delta);
+
+% ITEM c
+display(Gps0Delta - Gps0);
+display(Gpr0Delta - Gpr0);
+
+% ITEM d
 Gpf = 1;
 kp = 0.12;
 
@@ -40,7 +58,12 @@ GaDelta = Gpf * GpsDelta;
 Gf = Gpf * feedback(Gps * kp, 1);
 GfDelta = Gpf * feedback(GpsDelta * kp, 1);
 
-% item e
+display(tf(Ga));
+display(tf(GaDelta));
+display(tf(Gf));
+display(tf(GfDelta));
+
+% ITEM e
 Ea = (1 - Ga);
 EaDelta = (1 - GaDelta);
 Ef = (1 - Gf);
@@ -62,7 +85,7 @@ stepGaDelta = step(GaDelta);
 stepGf = step(Gf);
 stepGfDelta = step(GfDelta);
 
-% questao 4
+% QUESTAO 4
 kpfA = 1 / Gps0;
 GaZeroErr = kpfA * Gps;
 EaZeroErr = (1 - GaZeroErr);
@@ -70,7 +93,7 @@ eRegAZeroErr = dcgain(EaZeroErr);
 
 display(eRegAZeroErr);
 
-% questao 5
+% QUESTAO 5
 kpfF = (1 + kp * Gps0) / (kp * Gps0);
 GfZeroErr = kpfF * feedback(Gps * kp, 1);
 EfZeroErr = (1 - GfZeroErr);
